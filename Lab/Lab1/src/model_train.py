87396419069d1d733ff3b4baf1acd80eb4ddeabb
崂山摆摆 <harry.yilin.zhang@outlook.com>
2025-08-27 22:44:14 +0800

    intel version

--- Lab/Lab1/src/model_train.py
+++ Lab/Lab1/src/model_train.py
@@ -9,15 +9,18 @@ def train_model():
     print(f"Training model...Using device: {DEVICE}...")
 
     model = YOLO("weight/yolov8n.pt").to(DEVICE)   # Loading Pre-trained model
+    model = ipex.optimize(model)
+
+    device = torch.device(DEVICE)
 
     results = model.train(
         data = COMBINED_DATA_YAML,
         epochs = 100,
         imgsz = 640,
-        batch = 36,
+        batch = 42 ,
         name = COMBINED_MODEL_NAME,
-        device = DEVICE,
-        workers = 6,
+        device = device,
+        workers = 4,
         project = "./runs/train_combined",
         exist_ok = False,
         amp = False
