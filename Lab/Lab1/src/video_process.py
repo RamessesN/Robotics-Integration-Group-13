eecf0cb4a578d6d3a2869288e89ffcc715021439
RamessesN <stanley_zhao0113@outlook.com>
2025-08-29 00:59:41 +0800

    [stanley] add `resize` function for video process

--- Lab/Lab1/src/video_process.py
+++ Lab/Lab1/src/video_process.py
@@ -2,17 +2,20 @@ import cv2
 from obj_detect import validate_model
 from env_config import DEVICE
 
-def detect_from_video(model, video_path, output_path):
+def detect_from_video(model, video_path, output_path, target_res = (640, 360)):
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print(f"Cannot open video file: {video_path}")
         return
 
-    # Video config
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # Original video config
+    orig_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    orig_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    print(f"Video resolution: {width}x{height}, FPS: {fps}")
+    print(f"Video resolution: {orig_width}x{orig_height}, FPS: {fps}")
+
+    width, height = target_res
+    print(f"Target resolution: {width}x{height}")
 
     win_name = "on Live"
 
@@ -38,18 +41,21 @@ def detect_from_video(model, video_path, output_path):
             print("End of video or cannot read frame.")
             break
 
+        # Resize the RES of the video
+        frame = cv2.resize(frame, (width, height))
+
         current_time = cv2.getTickCount()
 
         results = model.predict(frame, conf=0.25, device=DEVICE, verbose=False)
         annotated_frame = results[0].plot()
 
-        fps_calc = cv2.getTickFrequency() / (current_time - prev_time)
+        fps_calc = cv2.getTickFrequency() / (current_time - prev_time) if prev_time else 0
         prev_time = current_time
 
-        cv2.putText(annotated_frame, f"Res: {width}x{height}", (10, 60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         cv2.putText(annotated_frame, f"FPS: {int(fps_calc)}", (10, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(annotated_frame, f"Res: {width}x{height}", (10, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         cv2.imshow(win_name, annotated_frame)
         out.write(annotated_frame)
@@ -69,4 +75,9 @@ def detect_from_video(model, video_path, output_path):
 if __name__ == "__main__":
     model = validate_model()
     if model is not None:
-        detect_from_video(model, "./video/input.mp4", "video/output_v1.mp4")
+        detect_from_video(
+            model,
+            "./video/input.mp4",
+            "video/output_v2.mp4",
+            target_res=(640, 360)
+        )
