eb814d164f0cc40e99dce9151846d08a780b1b1d
RamessesN <stanley_zhao0113@outlook.com>
2025-09-03 21:55:38 +0800

    [stanley] add a simple test file

--- Lab/Final Lab/test.py
+++ Lab/Final Lab/test.py
@@ -1,2 +1,44 @@
+import time
 from robomaster_ultra import robot
+from robomaster_ultra import led
 
+def robot_connect(ep_robot):
+    ep_robot.initialize(conn_type = "ap")
+
+def version_print(ep_robot):
+    version = ep_robot.get_version()
+    print("Robot version: {0}".format(version))
+
+def led_config(ep_robot):
+    ep_led = ep_robot.led
+    ep_led.set_led(
+        comp = led.COMP_ALL,
+        r = 255,
+        g = 255,
+        b = 0,
+        effect = led.EFFECT_ON
+    )
+
+def move_config(ep_robot):
+    ep_chassis = ep_robot.chassis
+    ep_chassis.drive_speed(
+        x = 0.5,
+        y = 0,
+        z = 0,
+        timeout = 5
+    )
+
+
+if __name__ == '__main__':
+    ep_robot = robot.Robot()
+    robot_connect(ep_robot)
+
+    ep_robot.set_robot_mode(mode = robot.CHASSIS_LEAD)
+
+    version_print(ep_robot)
+    led_config(ep_robot)
+    move_config(ep_robot)
+
+    time.sleep(3)
+
+    ep_robot.close()
